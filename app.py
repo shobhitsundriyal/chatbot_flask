9b7155a94e24782a2ff77d7021e59d46a6971d83
unknown <shobhitsundriyal0170@gmail.com>
2019-12-20 13:25:12 +0530

    inital commit

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
