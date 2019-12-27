841172880d073e629cc8e48b0b9be20b3179a710
unknown <shobhitsundriyal0170@gmail.com>
2019-12-27 22:16:45 +0530

    a

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
+english_bot = ChatBot('ChatterBot', storage_adapter='chatterbot.storage.SQLStorageAdapter')
 
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train('chatterbot.corpus.english')
